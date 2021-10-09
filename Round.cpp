80b6dec647f845d43667961c100d3ff6407fbecd
Afvan Moopen <40759954+AfvanMoopen@users.noreply.github.com>
2021-10-09 22:13:34 +0400

    Create Round.cpp

---
+++ Round.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int main() {
+int count,j, n, time, remain, flag = 0, time_quantum;
+printf("Enter total Process");
+scanf("%d", &n);
+remain=n;
+for(count= 0; count<n;count++) 
+{
+printf("Enter Arrival Time and Burst time for process process Number %d : ", count+1);
+scanf("%d", &at[count]);
+scanf("%d", &bt[count]);
+rt[count] = bt[count];
+
+}
+printf("Enter time quantum :\t");
+scanf("%d", &time_quantum);
+printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
+for(time = 0, count = 0; reamin != 0;)
+{
+
+}
+
+
+}
